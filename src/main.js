f582801ff6d1ce7fdb4dcee57e7b27b0ea85ea5d
Igranka <sirbusiness0@gmail.com>
2024-04-15 16:18:23 +0300

    2.3
    Fixed main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './js/reviews-main';
+
+import './js/animation-covers';
+
+import './js/projects-swiper';
+
+import './js/about-accordeon';
+
+import './js/faq';
+
+import './js/footer-form-submission';
+
+
