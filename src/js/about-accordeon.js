013b25dfb724b81e9a70e309545c4b8dcdef353f
ValeriiaPetryna <valeriapetrina1@gmail.com>
2024-04-15 12:45:39 +0300

    not  true commit

--- src/js/about-accordeon.js
+++ src/js/about-accordeon.js
@@ -1,21 +1,2 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-
-// // Default options
-// new Accordion('.accordion-container');
-
-// new Accordion('.accordion-container', {
-//   duration: 400,
-//   showMultiple: true,
-//   onOpen: function (currentElement) {
-//     console.log(currentElement);
-//   },
-// });
-// const accordions = Array.from(
-//   document.querySelectorAll('.accordion-container')
-// );
-// new Accordion(accordions, {});
-
-// // Detach events
-// const accordion = new Accordion('.container-first');
-// accordion.detachEvents();
