df507ac984ece02b3e363129159410d357ec1ce8
Anastasiia <anastasia.zhmurenko@gmail.com>
2024-04-16 20:57:53 +0300

    modal, mobile-menu - use icons.svg, flex css

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,25 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   const mobileMenuOpenBtn = document.querySelector('.mobile-menu-open-btn');
   const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
+  const body = document.querySelector('body');
 
   mobileMenuOpenBtn.addEventListener('click', function () {
     mobileMenuWrapper.classList.toggle('is-open');
-  });
-
-  const dropdownLinks = document.querySelectorAll('.dropdown');
-
-  dropdownLinks.forEach(function (link) {
-    link.addEventListener('click', function (event) {
-      if (window.innerWidth > 768) {
-        const dropdownContent = link.nextElementSibling;
-        dropdownContent.classList.toggle('is-open');
-      }
-    });
+    body.classList.toggle('modal-open');
   });
 
   const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
 
   mobileMenuCloseBtn.addEventListener('click', function () {
     mobileMenuWrapper.classList.remove('is-open');
+    body.classList.toggle('modal-open');
   });
 });
