b9f6c2d55e3b9d8d151925960901e991a585c01a
ValeriiaPetryna <valeriapetrina1@gmail.com>
2024-04-17 03:56:39 +0300

    акордеон працює
    треба пофіксити тільки щоб змінювалася кнопка з up на down і трошки може поїхав текст, теж треба розібратися + перша li має бути завжди відкрита

--- src/js/about-accordeon.js
+++ src/js/about-accordeon.js
@@ -1,21 +1,11 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
 
-// // Default options
-// new Accordion('.accordion-container');
-
-// new Accordion('.accordion-container', {
-//   duration: 400,
-//   showMultiple: true,
-//   onOpen: function (currentElement) {
-//     console.log(currentElement);
-//   },
-// });
-// const accordions = Array.from(
-//   document.querySelectorAll('.accordion-container')
-// );
-// new Accordion(accordions, {});
-
-// // Detach events
-// const accordion = new Accordion('.container-first');
-// accordion.detachEvents();
+const accordions = Array.from(
+  document.querySelectorAll('.accordion-container')
+);
+new Accordion(accordions, {
+  duration: 600,
+  showMultiple: false,
+  openOnInit: [0],
+});
