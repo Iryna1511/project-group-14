4d6d00e3f7c07935e00f8a5074e29513f01372e3
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-04-18 08:35:28 +0300

    up

--- src/js/about-swiper.js
+++ src/js/about-swiper.js
@@ -5,48 +5,79 @@ import 'swiper/css/navigation';
 // const swiper_2 = document.querySelector('.swiper-skills');
 const BUTTON_NEXT = document.querySelector('.btn-right');
 
-const swiper_2 = new Swiper('.swiper-skills', {
-  modules: [Navigation, Keyboard, Pagination],
+export const swiper_2 = new Swiper('.swiper-skills', {
+  loop: true,
+  setWrapperSize: true,
+  modules: [Navigation, Keyboard, Mousewheel],
+  spaceBetween: 0,
+  simulateTouch: true,
+  grabCursor: true,
+  slideActiveClass: 'active',
   navigation: {
     nextEl: BUTTON_NEXT,
-    prevEl: BUTTON_NEXT,
-    lockClass: '.swiper-lock-step-navs',
   },
-
-  // navigation: {
-  //   nextEl: BUTTON_NEXT,
-  //   prevEl: BUTTON_NEXT,
-  // },
-  // loop: true,
-  breakpoints: {
-    375: {
-      slidesPerView: 2,
-    },
-    767: {
-      slidesPerView: 3,
-    },
-    1280: {
-      slidesPerView: 3,
-    },
-    1440: {
-      slidesPerView: 'auto',
-    },
-  },
-  mousewheel: {
-    invert: true,
-  },
-  direction: 'horizontal',
-  // autoHeight: false,
-  // autoWidth: false,
-  loop: true,
-  // watchOverflow: true,
   keyboard: {
     enabled: true,
     onlyInViewport: false,
-    pageUpDown: true,
   },
-  //  return swiper_2();
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    375: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
 });
+
+// const swiper_2 = new Swiper('.swiper-skills', {
+//   modules: [Navigation, Keyboard, Pagination],
+//   navigation: {
+//     nextEl: BUTTON_NEXT,
+//     prevEl: BUTTON_NEXT,
+//     lockClass: '.swiper-lock-step-navs',
+//   },
+
+//   // navigation: {
+//   //   nextEl: BUTTON_NEXT,
+//   //   prevEl: BUTTON_NEXT,
+//   // },
+//   // loop: true,
+//   breakpoints: {
+//     375: {
+//       slidesPerView: 2,
+//     },
+//     767: {
+//       slidesPerView: 3,
+//     },
+//     1280: {
+//       slidesPerView: 3,
+//     },
+//     1440: {
+//       slidesPerView: 'auto',
+//     },
+//   },
+//   mousewheel: {
+//     invert: true,
+//   },
+//   direction: 'horizontal',
+//   // autoHeight: false,
+//   // autoWidth: false,
+//   loop: true,
+//   // watchOverflow: true,
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: false,
+//     pageUpDown: true,
+//   },
+//   //  return swiper_2();
+// });
 
 swiper_2.update();
 
