ed577d20d1a8b1dcd2d5d036dcec299bf50f302e
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-04-18 12:28:48 +0300

    about accordion btns
    +все чисто по секції, відсутні зайві коментарі

--- src/js/about-accordeon.js
+++ src/js/about-accordeon.js
@@ -1,20 +1,18 @@
 import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
 
 const accordions = Array.from(
   document.querySelectorAll('.accordion-container')
 );
 new Accordion(accordions, {
   duration: 600,
-  showMultiple: false,
+  showMultiple: true,
   openOnInit: [0],
 });
 
-const aboutListEl = document.querySelector('.about-list');
-const arrowBtn = document.querySelectorAll('.button-up');
-aboutListEl.addEventListener('click', event => {
-  console.log(event);
-  if (event.target === arrowBtn) {
-    arrowBtn.classList.toggle('rotated');
-  }
+const buttons = document.querySelectorAll('.ac-trigger');
+buttons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    const clickedButton = this;
+    clickedButton.classList.toggle('rotated');
+  });
 });
