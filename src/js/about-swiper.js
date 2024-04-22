20d60dba7ceb9f6e36f889c39473574b9ab0a04f
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-04-22 14:17:28 +0300

    up

--- src/js/about-swiper.js
+++ src/js/about-swiper.js
@@ -1,11 +1,11 @@
 import Swiper from 'swiper';
-import { Navigation, Keyboard, Pagination } from 'swiper/modules';
+import { Navigation, Keyboard, Pagination, Mousewheel } from 'swiper/modules';
 import 'swiper/css/navigation';
 
 // const swiper_2 = document.querySelector('.swiper-skills');
 const BUTTON_NEXT = document.querySelector('.btn-right');
 
-export const swiper_2 = new Swiper('.swiper-skills', {
+const swiper_2 = new Swiper('.swiper-skills', {
   loop: true,
   setWrapperSize: true,
   modules: [Navigation, Keyboard, Mousewheel],
@@ -13,8 +13,10 @@ export const swiper_2 = new Swiper('.swiper-skills', {
   simulateTouch: true,
   grabCursor: true,
   slideActiveClass: 'active',
+  pagination: true,
+  oneWayMovement: false,
   navigation: {
-    nextEl: BUTTON_NEXT,
+    nextEl: '.btn-right',
   },
   keyboard: {
     enabled: true,
@@ -36,66 +38,4 @@ export const swiper_2 = new Swiper('.swiper-skills', {
   },
 });
 
-// const swiper_2 = new Swiper('.swiper-skills', {
-//   modules: [Navigation, Keyboard, Pagination],
-//   navigation: {
-//     nextEl: BUTTON_NEXT,
-//     prevEl: BUTTON_NEXT,
-//     lockClass: '.swiper-lock-step-navs',
-//   },
-
-//   // navigation: {
-//   //   nextEl: BUTTON_NEXT,
-//   //   prevEl: BUTTON_NEXT,
-//   // },
-//   // loop: true,
-//   breakpoints: {
-//     375: {
-//       slidesPerView: 2,
-//     },
-//     767: {
-//       slidesPerView: 3,
-//     },
-//     1280: {
-//       slidesPerView: 3,
-//     },
-//     1440: {
-//       slidesPerView: 'auto',
-//     },
-//   },
-//   mousewheel: {
-//     invert: true,
-//   },
-//   direction: 'horizontal',
-//   // autoHeight: false,
-//   // autoWidth: false,
-//   loop: true,
-//   // watchOverflow: true,
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: false,
-//     pageUpDown: true,
-//   },
-//   //  return swiper_2();
-// });
-
 swiper_2.update();
-
-// const nextObserver = new MutationObserver(mutations => {
-//   mutations.forEach(mutation => {
-//     if (mutation.attributeName === 'class') {
-//       const disabled = BUTTON_NEXT.classList.contains(
-//         'swiper-button-next-disabled'
-//       );
-//       if (disabled) {
-//         BUTTON_NEXT.style.opacity = '0.5';
-//         BUTTON_NEXT.style.cursor = 'not-allowed';
-//       } else {
-//         BUTTON_NEXT.style.opacity = '1';
-//         BUTTON_NEXT.style.cursor = 'pointer';
-//       }
-//     }
-//   });
-// });
-
-// prevObserver.observe(BUTTON_NEXT, { attributes: true });
